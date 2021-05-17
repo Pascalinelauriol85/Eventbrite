9fe43849bc919cdb740347b8671f2e9254a6cdc2
pascalinelauriol85 <pascalinelauriol85@gmail.com>
2021-05-17 15:52:56 +0200

    version 1.0

---
+++ app/models/attendance.rb
@@ -0,0 +1,4 @@
+class Attendance < ApplicationRecord
+  belong_to :user
+  belong_to :event
+end
---
+++ db/seeds.rb
@@ -0,0 +1,39 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Event.destroy_all
+
+
+10.times do |i|
+  User.create(first_name:Faker::Games::Pokemon.name,
+    last_name:Faker::Games::SuperMario.character,
+    description:Faker::Movies::BackToTheFuture.quote,
+    email:"john.lolo#{i}@yopmail.com")
+  puts "#{i+1} users created"
+end
+
+
+10.times do |i|
+  Event.create(start_date:Faker::Date.between(from: Date.today, to:100.days.ago),
+  duration:[5,10,15,20,25,30,35,40,45].sample,
+  title:Faker::Movies::BackToTheFuture.quote,
+  description:Faker::TvShows::NewGirl.quote,
+  price:rand(1..1000),l
+  ocation:Faker::JapaneseMedia::Naruto.character,
+  admin_id:User.all.sample.id)
+  puts "#{i+1} events created"
+end
+
+
+50.times do |i|
+  Attendance.create(user_id:User.all.sample.id,
+    event_id:User.all.sample.id)
+    puts "#{i+1} attendance created"  
+end
